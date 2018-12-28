bcda1dce4613b7dcea8b53f5b145ae2885b5cc57
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2018-12-29 00:58:34 +0800

    Update ESP8266.ts

--- ESP8266.ts
+++ ESP8266.ts
@@ -27,9 +27,9 @@ namespace ESP8266_IoT {
         )
         basic.pause(10)
         serial.writeString("AT+CWMODE=1" + "\u000D" + "\u000A")
-        basic.pause(5000)
+        basic.pause(1000)
         serial.writeString("AT+RST" + "\u000D" + "\u000A")
-        basic.pause(5000)
+        basic.pause(1000)
     }
 
     /**
@@ -44,7 +44,7 @@ namespace ESP8266_IoT {
             + key
             + "\""
         serial.writeString(text + "\u000D" + "\u000A")
-        basic.pause(5000)
+        basic.pause(3000)
     }
 
     /**
@@ -57,7 +57,7 @@ namespace ESP8266_IoT {
         let text = "AT+CIPSTART=\"TCP\",\"" + ip + "\",80"
         serial.writeString(text + "\u000D" + "\u000A")
         URL = ip
-        basic.pause(5000)
+        basic.pause(3000)
     }
 
     /**
@@ -107,9 +107,9 @@ namespace ESP8266_IoT {
         text = "AT+CIPSEND="
             + (tobesendstring.length + 2)
         serial.writeString(text + "\u000D" + "\u000A")
-        basic.pause(2000)
+        basic.pause(1000)
         serial.writeString(tobesendstring + "\u000D" + "\u000A")
-        basic.pause(8000)
+        basic.pause(4000)
     }
 
 }
